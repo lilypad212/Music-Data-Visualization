1cfd412fac0f393e5c7de2174a5d917d41ddbdd1
Lily Wang <fwang@wellesley.edu>
2018-08-27 17:42:45 -0400

    Add files via upload

--- Music data.py
+++ Music data.py
@@ -1,6 +1,7 @@
 # Lily Wang
 # 8/22/18
-# Music data visualization pt 1: Creates an Excel sheet of song titles and artists of songs in your specific genre
+# Music data visualization pt 1: Creates an Excel sheet of song titles and artists of songs in a specific genre
+# as well as a text file of a list of unique artists
 
 import os
 import eyed3 #external package for reading metadata of audio files
@@ -10,7 +11,7 @@ import xlsxwriter
 eyed3.log.setLevel("ERROR") #hides warning messages
 
 
-def show_info():
+def show_info(): #test
     audio = eyed3.load("E:\\Music\Power Up.mp3")
     print audio.tag.artist
     print audio.tag.album
@@ -22,20 +23,26 @@ show_info()
 title = []
 artist = []
 gender = []
-
 dirname = "E:/Music"
+artistsList = open("C:\Users\Lily\Desktop\CS Programs\List of Artists.txt","w") #List of all unique artists in collection
+
+
 for name in os.listdir(dirname):
     path = os.path.join(dirname, name) # Path name for subfolders
-    if path.endswith('mp3'):
-        song = eyed3.load(path)
-        if str(song.tag.genre) == ("K-Pop"):
-            if song.tag.artist not in artist:
-                #artistsList.write(song.tag.artist)
-            title.append(song.tag.title)
-            artist.append(song.tag.artist)
+    if os.path.exists(path): 
+        if path.endswith('mp3'):
+            song = eyed3.load(path)
+            if str(song.tag.genre) == ("K-Pop"):
+                if song.tag.artist not in artist:
+                    artistsList.write(song.tag.artist.encode('utf-8'))
+                    artistsList.write('\n')
+                print song.tag.title
+                title.append(song.tag.title)
+                artist.append(song.tag.artist)
 
 out_path = "C:/Users/Lily/Desktop/CS Programs/Data.xlsx"
 df = pd.DataFrame({'Artist': artist, 'Song Title': title})
 writer = pd.ExcelWriter(out_path , engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Sheet1')
 writer.save()
+artistsList.close()
