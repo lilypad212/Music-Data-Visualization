4a3cc4c7dd1063927dab35e1d140016cc257a14b
Lily Wang <fwang@wellesley.edu>
2018-08-27 22:24:02 -0400

    Successful adding of title, artist, and artist gender

--- Music data.py
+++ Music data.py
@@ -11,7 +11,7 @@ import xlsxwriter
 eyed3.log.setLevel("ERROR") #hides warning messages
 
 
-def show_info(): #test
+def show_info(): #initial test
     audio = eyed3.load("E:\\Music\Power Up.mp3")
     print audio.tag.artist
     print audio.tag.album
@@ -22,26 +22,43 @@ show_info()
 
 title = []
 artist = []
+#year = []
 gender = []
 dirname = "E:/Music"
 artistsList = open("C:\Users\Lily\Desktop\CS Programs\List of Artists.txt","w") #List of all unique artists in collection
 
+def add_title_artist():
+    for name in os.listdir(dirname):
+        path = os.path.join(dirname, name) # Path name for subfolders
+        if os.path.exists(path): 
+            if path.endswith('mp3'): #Find all mp3 files
+                song = eyed3.load(path)
+                if str(song.tag.genre) == ("K-Pop"): #Only adding K-Pop songs
+                    if song.tag.artist not in artist:
+                        artistsList.write(song.tag.artist.encode('utf-8')) #Makes a list of unique artists
+                        artistsList.write('\n')
+                    #print song.tag.title
+                    title.append(song.tag.title)
+                    artist.append(song.tag.artist)
+                    #year.append(song.tag.getBestDate())
 
-for name in os.listdir(dirname):
-    path = os.path.join(dirname, name) # Path name for subfolders
-    if os.path.exists(path): 
-        if path.endswith('mp3'):
-            song = eyed3.load(path)
-            if str(song.tag.genre) == ("K-Pop"):
-                if song.tag.artist not in artist:
-                    artistsList.write(song.tag.artist.encode('utf-8'))
-                    artistsList.write('\n')
-                print song.tag.title
-                title.append(song.tag.title)
-                artist.append(song.tag.artist)
+add_title_artist()
+
+#Defines each song as by a "male", "female", or "other" artist. Other means both female and male (i.e. duet or co-ed group)
+def add_gender():
+    for name in artist:
+        if name.encode('utf-8').upper() in open('C:\Users\Lily\Desktop\CS Programs\Female Artists.txt').read():
+            gender.append("Female")
+        elif name.encode('utf-8').upper() in open('C:\Users\Lily\Desktop\CS Programs\Male Artists.txt').read():
+            gender.append("Male")
+        else:
+            gender.append("Other")
+
+add_gender()
 
 out_path = "C:/Users/Lily/Desktop/CS Programs/Data.xlsx"
-df = pd.DataFrame({'Artist': artist, 'Song Title': title})
+df = pd.DataFrame({'Song Title': title, 'Artist': artist, 'Gender': gender})
+df = df[['Song Title','Artist','Gender']]
 writer = pd.ExcelWriter(out_path , engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Sheet1')
 writer.save()
